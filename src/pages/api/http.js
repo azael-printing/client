096ed89ba2cea2d8bd5428f78bee9e7ad2eaf917
semahegn-tilahun <yegnabetsew@gmail.com>
2026-03-22 06:00:23 +0300

    update azaelprintingservice

--- src/pages/api/http.js
+++ src/pages/api/http.js
@@ -1,23 +1,3 @@
-// import axios from "axios";
-
-// export const http = axios.create({
-//   baseURL: import.meta.env.VITE_API_URL || "https://azclient.vercel.app/",
-// });
-
-// export function setAuthToken(token) {
-//   if (token) http.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-//   else delete http.defaults.headers.common["Authorization"];
-// }
-
-// http.interceptors.request.use(
-//   (config) => {
-//     const token = localStorage.getItem("accessToken");
-//     config.headers = config.headers ?? {};
-//     if (token) config.headers["Authorization"] = `Bearer ${token}`;
-//     return config;
-//   },
-//   (error) => Promise.reject(error),
-// );
 import axios from "axios";
 
 export const http = axios.create({
